3618cc9571f6c5f5742e01204499c048ed30784c
Heri <hersac1@gmail.com>
2024-03-18 14:06:40 -0500

    Se integran cambios en los eventos de os botones

--- FastFoot/src/main/java/com/hersac/fastfoot/FastFoot.java
+++ FastFoot/src/main/java/com/hersac/fastfoot/FastFoot.java
@@ -1,7 +1,11 @@
 package com.hersac.fastfoot;
 
+import com.hersac.fastfoot.ui.Root;
+
 public class FastFoot {
-    
+
     public static void main(String[] args) {
+        Root root = new Root();
+        root.setVisible(true);
     }
 }
--- FastFoot/src/main/java/com/hersac/fastfoot/services/ClientesService.java
+++ FastFoot/src/main/java/com/hersac/fastfoot/services/ClientesService.java
@@ -5,30 +5,30 @@ import com.hersac.fastfoot.repositories.ClientesRepositorio;
 import java.util.List;
 
 public class ClientesService {
-    
+
     private ClientesRepositorio clientesRepo = new ClientesRepositorio();;
-    
-    public List<Clientes> getClientes(){
+
+    public List<Clientes> getClientes() {
         return this.clientesRepo.findAll();
     }
-    
-    public Clientes getClienteById(Long id){
+
+    public Clientes getClienteById(Long id) {
         return this.clientesRepo.findById(id);
     }
-    
-    public Clientes addClienteById(Clientes cliente){
+
+    public Clientes addCliente(Clientes cliente) {
         return this.clientesRepo.save(cliente);
     }
-    
-    public Clientes updateCliente(Long id, Clientes cliente){
+
+    public Clientes updateCliente(Long id, Clientes cliente) {
         return this.clientesRepo.update(id, cliente);
     }
-    
-    public void deleteClienteById(Long id){
+
+    public void deleteClienteById(Long id) {
         this.clientesRepo.deleteById(id);
     }
-    
-    public Clientes findClientesByIdentificacion(String identificacion){
+
+    public Clientes findClientesByIdentificacion(String identificacion) {
         return this.clientesRepo.findByNumeroDocumento(identificacion);
     }
 }
